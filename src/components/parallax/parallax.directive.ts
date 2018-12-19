8f90ef97cc8b346e671a3d599594133e65792ffd
Miguel A. Marcos <miguelangel.marcos@samelan.com>
2018-12-19 11:36:00 +0100

    button parallax

--- src/components/parallax/parallax.directive.ts
+++ src/components/parallax/parallax.directive.ts
@@ -1,11 +1,11 @@
-import { Content } from 'ionic-angular';
-import { Directive, ElementRef, Input, Renderer2, SimpleChanges } from '@angular/core';
+import { Content, Button } from 'ionic-angular';
+import { Directive, ElementRef, Input, Renderer2, SimpleChanges, AfterViewInit } from '@angular/core';
 
 
 @Directive({
     selector: '[scrollHide]'
 })
-export class ScrollHideDirective {
+export class ScrollHideDirective implements AfterViewInit {
 
     @Input('scrollHide') config: ScrollHideConfig;
     @Input('scrollContent') scrollContent: Content;
@@ -14,8 +14,18 @@ export class ScrollHideDirective {
     scrollHeight: number;
     lastScrollPosition: number;
     lastValue: number = 0;
+    lastValueButton: number = 105;
+
+    button: HTMLElement;
 
     constructor(private element: ElementRef, private renderer: Renderer2) {
+    }
+
+    ngAfterViewInit() {
+        this.button = document.getElementsByClassName('ion-md-leerQr')[0] as HTMLElement;
+        this.button.style.transformOrigin = 'bottom center';
+        this.button.style.paddingBottom = '5px';
+        console.log(this.button);
     }
 
     ngOnChanges(changes: SimpleChanges) {
@@ -39,15 +49,22 @@ export class ScrollHideDirective {
                 let scrollTop: number = ev.scrollTop > 0 ? ev.scrollTop : 0;
                 let scrolldiff: number = (scrollTop - this.lastScrollPosition) / 1.75;
                 this.lastScrollPosition = scrollTop;
+
                 let newValue = this.lastValue + scrolldiff;
+                let newValueButton = this.lastValueButton - scrolldiff;
                 newValue = Math.max(0, Math.min(newValue, this.config.maxValue));
+                newValueButton = Math.max(64, Math.min(newValueButton, 105));
+
                 if (newValue === this.config.maxValue) {
                     this.renderer.addClass(this.element.nativeElement.children[0].children[0], 'compact');
                 } else {
                     this.renderer.removeClass(this.element.nativeElement.children[0].children[0], 'compact');
                 }
                 this.renderer.setStyle(this.element.nativeElement, this.config.cssProperty, `-${newValue}px`);
+                this.button.style.width = `${newValueButton}px`;
+
                 this.lastValue = newValue;
+                this.lastValueButton = newValueButton;
             });
         }
     }
