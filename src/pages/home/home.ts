ab48ac1f8d4b60c62b9977d0b3197245895f2e88
Miguel A. Marcos <miguelangel.marcos@samelan.com>
2019-01-08 08:47:38 +0100

    secured Storage update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,59 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { TabsPage } from '../tabsPage/tabsPage';
 import { RegisterHub } from '../register/register-hub/register-hub';
-import { AlastriaPublicKeyRegistryService } from '../../services/alastriaPublicKeyRegistry.service';
 
-import * as keypair from 'keypair';
+import { SessionSecuredStorageService } from '../../services/securedStorage.service';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio';
 
 @Component({
     selector: 'page-home',
     templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
     login: any = {};
     tabs: any = {};
     isLoged: Boolean;
+    isRegistered: Boolean;
 
     constructor(
         public navCtrl: NavController,
-        private alastriaPublicKeyRegistry: AlastriaPublicKeyRegistryService
-    ) {
-        let user = sessionStorage.getItem("loginName");
-        if (!user) {
-            this.isLoged = false;
-            this.setLoginParams();
-        } else {
-            this.isLoged = true;
-            this.navCtrl.setRoot(TabsPage);
-        }
+        private sessionSecuredStorageService: SessionSecuredStorageService,
+        private faio: FingerprintAIO
+    ) { }
 
-        // Test
-        this.alastriaPublicKeyRegistry.getAccountInfo().then(
-            async (succ: any) => {
-                console.log('Test ok!', succ);
-                // const publicKeyInit = 'Hola currito, soy una clave publica!';
-                const publicKeyInit = keypair();
-                console.log(publicKeyInit);
-
-                let status = await this.alastriaPublicKeyRegistry.registryStatus(succ.fromAccount);
-                const publicKey = this.alastriaPublicKeyRegistry.toUtf8(status)
-                console.log('Status: ', publicKey);
-
-                if (!this.alastriaPublicKeyRegistry.isStatusDefined(status)) {
-                    let result = await this.alastriaPublicKeyRegistry.registrySet(publicKeyInit.public, succ.fromAccount);
-                    console.log('Status: ', result);
-                } else {
-                    let statusObj = await this.alastriaPublicKeyRegistry.registryStatusObject(succ.fromAccount, publicKey);
-                    console.log('Status obj: ', statusObj);
-                }
-
+    async ngOnInit(): Promise<void> {
+        // Is session registered
+        this.setLoginParams();
+        this.sessionSecuredStorageService.isRegistered().then(
+            () => {
+                this.isRegistered = true;
+                this.regFinger().then(
+                    () => {
+                        this.isLoged = true;
+                        this.navCtrl.setRoot(TabsPage);
+                    }
+                ).catch(
+                    () => {
+                        this.isLoged = false;
+                    }
+                )
             },
             err => {
-                console.error('Test error!', err);
+                console.error(err);
             }
-        );
+        )
+
+        // Test
+        // this.alastriaPublicKeyRegistry.getAccountInfo().then(
+        //     async (succ: any) => {
+        //         console.log('Test ok!', succ);
+        //         // const publicKeyInit = 'Hola currito, soy una clave publica!';
+        //         const publicKeyInit = keypair();
+        //         console.log(publicKeyInit);
+
+        //         let status = await this.alastriaPublicKeyRegistry.registryStatus(succ.fromAccount);
+        //         const publicKey = this.alastriaPublicKeyRegistry.toUtf8(status)
+        //         console.log('Status: ', publicKey);
+
+        //         if (!this.alastriaPublicKeyRegistry.isStatusDefined(status)) {
+        //             let result = await this.alastriaPublicKeyRegistry.registrySet(publicKeyInit.public, succ.fromAccount);
+        //             console.log('Status: ', result);
+        //         } else {
+        //             let statusObj = await this.alastriaPublicKeyRegistry.registryStatusObject(succ.fromAccount, publicKey);
+        //             console.log('Status obj: ', statusObj);
+        //         }
+
+        //     },
+        //     err => {
+        //         console.error('Test error!', err);
+        //     }
+        // );
     }
 
     onLogin(params: any) {
@@ -107,5 +123,29 @@ export class HomePage {
 
             }
         };
+    }
+
+    regFinger() {
+        return new Promise(
+            (next, reject) => {
+                this.faio.isAvailable().then(result => {
+                    this.faio.show({
+                        clientId: "AlastriaID",
+                        clientSecret: "NddAHBODmhACXHITWJTU",
+                        disableBackup: true,
+                        localizedFallbackTitle: 'Touch ID for AlastriaID', //Only for iOS
+                    }).then(result => {
+                        next('Ok!');
+                    }).catch(err => {
+                        reject('Error in fingerprint');
+                    });
+                }).catch(err => {
+                    if (err === "cordova_not_available") {
+                        reject('Cordova not aviable');
+                    }
+                });
+            }
+        )
+
     }
 }
