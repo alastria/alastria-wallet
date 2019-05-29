84d72445eb56da6f610dee12bb2f5be9b38f0807
Álvaro Gómez Haro <agomez@tribalyte.com>
2019-05-29 11:53:56 +0200

    Added token magement service

--- src/pages/tabsPage/camera/camera.ts
+++ src/pages/tabsPage/camera/camera.ts
@@ -4,9 +4,9 @@ import { ToastService } from '../../../services/toast-service';
 import { TabsService } from '../../../services/tabs-service';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { AlertController, NavController, PopoverController, ModalController } from 'ionic-angular';
-import { verify } from 'jsonwebtoken';
 import { ConfirmLogin } from '../../confirmLogin/confirmLogin';
 import { Index } from '../index';
+import { TokenService } from '../../../services/token-service';
 
 @IonicPage()
 @Component({
@@ -25,7 +25,8 @@ export class Camera {
     public alertCtrl: AlertController,
     public navCtrl: NavController,
     public popoverCtrl: PopoverController,
-    public modalCtrl: ModalController) {
+    public modalCtrl: ModalController,
+    private tokenSrv: TokenService) {
     this.data = {
       title: "Cámara",
       format: "Escaneo de QRCodes blockchain",
@@ -41,34 +42,18 @@ export class Camera {
       let alastriaToken = barcodeData.text;
       let secret = "your-256-bit-secret";
 
-      let verifyJWT = verify(alastriaToken, secret, { algorithms: ['HS256'] });
+      let verifiedJWT = this.tokenSrv.verifyToken(alastriaToken, secret);
 
-      let currDate: any = new Date();
-      currDate = currDate.getTime();
+      let tokenType = this.tokenSrv.getTokenType(verifiedJWT);
 
-      let iss = verifyJWT["iss"];
-      let issName;
-      let cbu;
-
-      if (iss){
-        console.log(verifyJWT);
-        issName = "SERVICE PROVIDER";
-        cbu = verifyJWT["cbu"];
-
-        let alastriaSession = {
-          "iss": "did:ala:quor:telsius:0x123ABC",
-          "pku": secret,
-          "iat": currDate,
-          "exp": currDate + 5 * 60 * 1000,
-          "nbf ": currDate,
-          "data": verifyJWT
-        }
-      
-        this.showAlert(iss, issName, cbu, alastriaSession);
-      }else{
-        this.toastCtrl.presentToast("Error: Contacte con el service provider", 1000);
+      switch (tokenType){
+        case "authentication":
+            this.authProtocol(verifiedJWT, secret);
+            break;
+        case "credentialRequest":
+            this.credentialProtocol(verifiedJWT, secret);
+            break;
       }
-
     }).catch(err => {
       console.log('Error', err);
       this.qrCode = "hola";
@@ -77,9 +62,32 @@ export class Camera {
     });
   }
 
-  showAlert(iss: string, issName: string, cbu: string, as: string | object ) {
+  private showAlert(iss: string, issName: string, cbu: string, as: string | object ) {
     const alert = this.modalCtrl.create(ConfirmLogin, {"iss": iss, "issName": issName, "cbu": cbu, "as": as});
     alert.present();
   }
 
+  private authProtocol(verifiedToken: string|object, secret: string) {
+    let alastriaSession;
+
+    if (verifiedToken){
+      alastriaSession = this.tokenSrv.getSessionToken(verifiedToken);
+      this.showAlert(verifiedToken["iss"], "SERVICE PROVIDER", verifiedToken["cbu"], alastriaSession);
+    
+    }else{
+      this.toastCtrl.presentToast("Error: Contacte con el service provider", 1000);
+    }
+  }
+
+  private credentialProtocol(verifiedToken: string|object, secret: string) {
+    let alastriaSession;
+
+    if (verifiedToken){
+      alastriaSession = this.tokenSrv.getSessionToken(verifiedToken);
+      this.showAlert(verifiedToken["iss"], "SERVICE PROVIDER", verifiedToken["cbu"], alastriaSession);
+    
+    }else{
+      this.toastCtrl.presentToast("Error: Contacte con el service provider", 1000);
+    }
+  }
 }
