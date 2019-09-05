97b4a2bd76d4cd556e5e9d953bcb0a4d4cf024bd
Álvaro Gómez Haro <agomez@tribalyte.com>
2019-09-05 11:38:19 +0200

    Added presentation repo and fixes

--- src/pages/confirm-access/confirm-access.ts
+++ src/pages/confirm-access/confirm-access.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { ViewController, NavParams, ModalController } from 'ionic-angular';
+import { ViewController, NavParams, ModalController, NavController } from 'ionic-angular';
 import { ToastService } from '../../services/toast-service';
 import { IdentitySecuredStorageService } from '../../services/securedStorage.service';
 import { LoadingService } from '../../services/loading-service';
+import { TabsPage } from '../tabsPage/tabsPage';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 
 @Component({
@@ -14,6 +16,7 @@ export class ConfirmAccess {
     public dataNumberAccess: number;
     public isPresentationRequest: boolean;
     public issName: string;
+    public issDID: string;
 
     private readonly CREDENTIAL_PREFIX = "cred_";
     private readonly PRESENTATION_PREFIX = "present_";
@@ -24,13 +27,15 @@ export class ConfirmAccess {
     constructor(
         public viewCtrl: ViewController,
         public navParams: NavParams,
+        public navCtrl: NavController,
         public modalCtrl: ModalController,
         public toastCtrl: ToastService,
         private securedStrg: IdentitySecuredStorageService,
         private loadingSrv: LoadingService
     ) {
         this.dataNumberAccess = this.navParams.get("dataNumberAccess");
-        this.issName = this.navParams.get("issName");
+        this.issName = "SERVICE PROVIDER";
+        this.issDID = this.navParams.get("iss");
         this.credentials = this.navParams.get("credentials");
         this.isPresentationRequest = this.navParams.get("isPresentationRequest");
     }
@@ -50,18 +55,61 @@ export class ConfirmAccess {
     }
 
     private sendPresentation() {
-        let securedCredentials;
 
-        let credentialPromises = this.identitySelected.map((element) => {
-            let index = element;
-            return this.securedStrg.getJSON(this.CREDENTIAL_PREFIX + this.credentials[index]["field_name"]);
-        });
+        if (this.identitySelected.length === this.credentials.length) {
 
-        Promise.all(credentialPromises)
-            .then((result) => {
-                console.log(result);
-                securedCredentials = result;
+            let securedCredentials;
+
+            let credentialExistsPromises = this.identitySelected.map((element) => {
+                let index = element;
+                return this.securedStrg.hasKey(this.CREDENTIAL_PREFIX + this.credentials[index]["field_name"]);
             });
+
+            Promise.all(credentialExistsPromises)
+                .then((result) => {
+                    let allCredentialsAssigned = !result.some(element => !element);
+                    return Promise.resolve(allCredentialsAssigned);
+                })
+                .then((result) => {
+                    if (result === true) {
+                        this.showLoading();
+                        let credentialPromises = this.identitySelected.map((element) => {
+                            let index = element;
+                            return this.securedStrg.getJSON(this.CREDENTIAL_PREFIX + this.credentials[index]["field_name"]);
+                        });
+
+                        Promise.all(credentialPromises)
+                            .then((result) => {
+                                securedCredentials = result;
+
+                                let iat = new Date(this.navParams.get("iat") * 1000);
+                                let exp = new Date(this.navParams.get("exp") * 1000);
+                                let iatString = iat.getDay() + "/" + (iat.getMonth() + 1) + "/" + iat.getFullYear();
+                                let expString = exp.getDay() + "/" + (exp.getMonth() + 1) + "/" + exp.getFullYear();
+
+                                let presentation = {
+                                    "@context": "https://w3id.org/credentials/v1",
+                                    "jti": "https://www.metrovacesa.com/alastria/credentials/3732",
+                                    "iss": this.issDID,
+                                    "sub": "did:alastria:quorum:testnet1:QmeeasCZ9jLbX...ueBJ7d7csxhb",
+                                    "iat": iatString,
+                                    "exp": expString,
+                                    "nbf": iat,
+                                    "credentials": securedCredentials
+                                }
+                                
+                                this.securedStrg.setJSON(this.PRESENTATION_PREFIX + this.navParams.get("jti"), presentation)
+                                .then(() => {
+                                    this.showSucces();
+                                });
+                            });
+                    } else {
+                        this.toastCtrl.presentToast("Uno o mas campos de los solicitados estan vacios", 3000);
+                    }
+                });
+        } else {
+            this.toastCtrl.presentToast("Por favor seleccione todas las credenciales solicitadas", 3000);
+        }
     }
 
     private saveCredentials() {
@@ -78,7 +126,7 @@ export class ConfirmAccess {
                 let currentCredentialValue;
 
                 let finalCredential = this.credentials[index];
-                finalCredential.issuer = this.issName;
+                finalCredential.issuer = this.issDID;
 
                 let credentialPromise = this.securedStrg.hasKey(currentCredentialKey)
                     .then(result => {
@@ -107,7 +155,6 @@ export class ConfirmAccess {
             Promise.all(credentialPromises)
                 .then(() => {
                     this.showSucces();
-                    this.viewCtrl.dismiss();
                 });
 
         } else {
@@ -131,5 +178,11 @@ export class ConfirmAccess {
 
     public showSucces() {
         this.loadingSrv.updateModalState();
+        this.viewCtrl.dismiss();
+    }
+
+    public dismiss() {
+        this.navCtrl.setRoot(TabsPage);
+        this.viewCtrl.dismiss();
     }
 }
