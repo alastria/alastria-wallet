1a596b1e8a2cd7f33393b4fbac1959e5a8b8b3bf
iruiz <iruiz@tribalyte.com>
2019-09-18 13:40:59 +0200

    Adapted select-identity

--- src/pages/confirm-access/select-identity/select-identity.ts
+++ src/pages/confirm-access/select-identity/select-identity.ts
@@ -1,27 +1,30 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild, forwardRef } from '@angular/core';
 import { NavParams, NavController } from 'ionic-angular';
-import { IdentitySecuredStorageService } from '../../../services/securedStorage.service';
-
+import { IdentityDataListComponent } from '../../../components/identity-data-list/identity-data-list';
 
 @Component({
     selector: 'select-identity',
     templateUrl: 'select-identity.html'
 })
 export class SelectIdentity {
+    @ViewChild(forwardRef(() => IdentityDataListComponent))
+    identityDataList: IdentityDataListComponent;
+
     @Output() handleSelectNewCredential = new EventEmitter();
 
     private readonly CREDENTIAL_PREFIX = "cred_";
     private readonly PRESENTATION_PREFIX = "present_";
 
-    private identitySelected: Array<number> = [];
-    private credentials: Array<any>;
+    private identityReplaced: number;
+    private selectedMockCredential: any;
 
-    public searchTerm: string;
+    public searchTerm = "";
 
     constructor(
         public navParams: NavParams,
         public navCtrl: NavController,
-    ) { }
+    ) {
+    }
 
     onStarClass(items: any, index: number, e: any) {
         for (var i = 0; i < items.length; i++) {
@@ -32,15 +35,37 @@ export class SelectIdentity {
     onSearch(event?: any) {
         console.log('Event ', event);
         console.log('searchTerm', this.searchTerm);
+        try {
+            this.identityDataList.identityDisplay = this.identityDataList.identityData.filter(mock => {
+                if (mock.titleP.toLowerCase().indexOf(this.searchTerm.toLowerCase()) !== -1
+                    || mock.value.toLowerCase().indexOf(this.searchTerm.toLowerCase()) !== -1) {
+                    return mock;
+                }
+            });
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     handleIdentitySelect(event: any) {
-        console.log('handleIdentitySelect ', event);
+        if (event && event.value) {
+            this.selectedMockCredential = event.data;
+        } else {
+            this.selectedMockCredential = undefined;
+        }
+        console.log('Selected Credential: ', this.selectedMockCredential);
+    }
+
+    sortCredentials() {
+        this.identityDataList.identityDisplay.reverse();
     }
 
     accept() {
-        this.handleSelectNewCredential.emit(this.identitySelected);
-        this.navCtrl.pop();
+        const result: any = {
+            credential: this.identityDataList.credentials[this.identityDataList.chosenIndex],
+            mock: this.selectedMockCredential
+        }
+        this.navCtrl.pop().then(() => this.navParams.get("resolve")(result))
     }
 
     cancel() {
