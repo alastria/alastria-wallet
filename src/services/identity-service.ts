d328555aac3405c914d3486e1a40162241e235f8
Ronny <rdemera@tribalyte.com>
2019-11-29 13:33:10 +0100

    Changes on Identity Service

--- src/services/identity-service.ts
+++ src/services/identity-service.ts
@@ -1,38 +1,42 @@
 import { Injectable } from "@angular/core";
 import * as keythereum from "keythereum"
-import { HttpClient } from "@angular/common/http";
-import { UserIdentity } from "alastria-identity-lib"
+import { UserIdentity } from "alastria-identity-lib";
+import { SubjectCredential } from "../models/subject-credential.model";
+import { KeyStore } from "../keystore";
+import { AppConfig } from "../app.config";
+import { Web3Service } from "./web3-service";
+import * as Web3 from "web3";
 
 @Injectable()
 export class IdentityService {
 
-    private configData: any;
-
     private subjectPublicKey: string;
     private subjectPrivateKey: string;
-    private subjectIdentity: any;
+    private subjectIdentity: UserIdentity;
+    private web3: Web3;
 
-    public constructor(private http: HttpClient) { }
-
-    public init(web3: any) {
-        this.http.get("../assets/app-config.json").subscribe(response => {
-            this.configData = response;
-            let identityKeystore = this.configData.identityKeystore
-
-            try {
-                this.subjectPrivateKey = keythereum.recover(this.configData.addressPassword, identityKeystore)
-            } catch (error) {
-                console.log("ERROR: ", error)
-            }
-            this.subjectIdentity = new UserIdentity(web3, `0x${identityKeystore.address}`, this.subjectPrivateKey, null);
-
-            console.log("IdentityService initialized");
-        });
-
+    constructor(
+        private web3Srv: Web3Service,
+    ) {
+        this.web3 = web3Srv.getWeb3();
+        this.init();
     }
 
     public getPrivateKey(): string {
         return this.subjectPrivateKey;
     }
 
+    public getKnownTransaction(subjectCredential: SubjectCredential) {
+        return this.subjectIdentity.getKnownTransaction(subjectCredential);
+    }
+
+    private init() {
+        let identityKeystore = KeyStore.identityKeystore;
+        try {
+            this.subjectPrivateKey = keythereum.recover(AppConfig.addressPassword, identityKeystore)
+        } catch (error) {
+            console.log("ERROR: ", error)
+        }
+        this.subjectIdentity = new UserIdentity(this.web3, `0x${identityKeystore.address}`, this.subjectPrivateKey, null);
+    }
 }
