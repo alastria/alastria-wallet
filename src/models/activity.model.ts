0cd0dc5eaabb66290ae9731258f604aad2bceff7
xwu <xwu@tribalyte.com>
2020-01-30 16:28:29 +0100

    Adding activity status (frontend)

--- src/models/activity.model.ts
+++ src/models/activity.model.ts
@@ -1,3 +1,5 @@
+import { AppConfig } from "../app.config"
+
 export class ActivityM {
     activityId: number;
     title: string;
@@ -5,4 +7,5 @@ export class ActivityM {
     description: string;
     datetime: string;
     type: string;
+    status: AppConfig.ActivityStatus;
 }
