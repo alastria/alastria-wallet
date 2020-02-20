3e2c8863e75c459831c544735aa4b57a893fda5e
Samuel <sanchezlopezsamuel@gmail.com>
2020-02-20 14:17:26 +0100

    fix loadigModal dismiss

--- src/services/loading-service.ts
+++ src/services/loading-service.ts
@@ -27,7 +27,9 @@ export class LoadingService {
     }
 
     public hide() {
-        this.loadingModal.dismiss();
+        if (this.loadingModal) {
+            this.loadingModal.dismiss();
+        }
     }
 
     public showModal() {
