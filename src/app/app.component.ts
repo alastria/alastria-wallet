ae9c0ab3d12d31dce480065a9f1d70fca64863a7
Samuel <sanchezlopezsamuel@gmail.com>
2020-03-12 12:15:34 +0100

    refactor app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { Platform, App, Config } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+// Pages - Components
 import { HomePage } from '../pages/home/home';
-import { IdentityService } from '../services/identity-service';
-import { Web3Service } from '../services/web3-service';
-import { TransactionService } from '../services/transaction-service';
-import { AppConfig } from "../app.config";
 
 @Component({
     templateUrl: 'app.html'
@@ -14,8 +12,7 @@ import { AppConfig } from "../app.config";
 export class MyApp {
     rootPage: any = HomePage;
 
-    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, app: App, web3Srv: Web3Service, identitySrv: IdentityService,  transactionSrv: TransactionService) {
-        console.log("[Debug] App enter");
+    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
         platform.ready().then(() => {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
@@ -23,8 +20,6 @@ export class MyApp {
                 statusBar.backgroundColorByHexString('#325b8e');
             }
             splashScreen.hide();
-            let web3 = web3Srv.getWeb3();
-            console.log("AppConfig "  + JSON.stringify(AppConfig.credentialKey));
         });
     }
 }
