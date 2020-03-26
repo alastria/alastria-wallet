2cb3413640aa298ae4ce0dfc4cae3d5ed60c3a9d
Samuel <sanchezlopezsamuel@gmail.com>
2020-03-26 15:48:26 +0100

    identity data list refactor

--- src/components/identity-data-list/identity-data-list.ts
+++ src/components/identity-data-list/identity-data-list.ts
@@ -2,28 +2,9 @@ import { CredentialDetailPage } from '../../pages/credential-detail/credential-d
 import { Component, Input, EventEmitter, Output } from '@angular/core';
 import { NavParams, NavController } from 'ionic-angular';
 import { SecuredStorageService } from '../../services/securedStorage.service';
-import { SelectIdentity } from '../../pages/confirm-access/select-identity/select-identity';
-import { TokenService } from '../../services/token-service';
+import { SelectIdentity } from '../../pages/confirm-access/select-identity/select-identity'
 import { AppConfig } from '../../app.config';
-
-export interface mockCredential {
-    id: number,
-    titleP: string,
-    emitter: string,
-    place: string,
-    valueT: string,
-    value: string,
-    addDateT: string,
-    addDate: string,
-    endDateT: string,
-    endDate: string,
-    level: string,
-    iconsStars: Array<any>,
-    credentialAssigned: boolean,
-    isExpanded: boolean,
-    isHidden: boolean,
-    isChecked: boolean;
-}
+import { Identity } from '../../models/identity.model'
 
 @Component({
     selector: 'identity-data-list',
@@ -48,7 +29,7 @@ export class IdentityDataListComponent {
     public loadCredential = new EventEmitter();
 
     public credentials: any[];
-    public identityDisplay = new Array<mockCredential>();
+    public identityDisplay = new Array<Identity>();
     public chosenIndex: number;
     public isDataSetted = false;
     public isOrderInverted = false;
@@ -59,171 +40,63 @@ export class IdentityDataListComponent {
         public navCtrl: NavController,
         public navParams: NavParams,
         private securedStrg: SecuredStorageService,
-        private tokenSrv: TokenService
     ) {
-
     }
 
     ngOnInit() {
         this.isPresentationRequest = this.navParams.get(AppConfig.IS_PRESENTATION_REQ);
+        this.parseAllCredentials();
+    }
 
-        let iat: any;
-        let exp: any;
+    private async parseAllCredentials(): Promise<void> {
         let iatString: any; 
         let expString: any;
         if (this.isManualSelection) {
             this.credentials = this.allCredentials.map(cred => JSON.parse(cred));
-            console.log('Credentals in identity list --> ', this.credentials);
-            if (this.iat) {
-                iat = new Date(this.iat * 1000);
-                iatString = iat.getDay() + "/" + (iat.getMonth() + 1) + "/" + iat.getFullYear();
-            } else {
-                iatString = "";
-            }
-
-            if (this.exp) {
-                exp = new Date(this.exp * 1000);
-                expString = exp.getDay() + "/" + (exp.getMonth() + 1) + "/" + exp.getFullYear();
-            } else {
-                expString = "";
-            }
+            iatString = this.parseFormatDate(this.iat);
+            expString = this.parseFormatDate(this.exp);
         } else {
             this.credentials = this.navParams.get(AppConfig.CREDENTIALS);
         }
         let count = 0;
-        let credentialPromises = this.credentials.map((credential) => {
+        let credentialPromises = this.credentials.map(async (credential) => {
             let propNames = Object.getOwnPropertyNames(credential);
 
-            let level = credential.levelOfAssurance;
-            switch (level){
-                case AppConfig.LevelOfAssurance.Self:
-                    level = 0;
-                    break;
-                case AppConfig.LevelOfAssurance.Low:
-                    level = 1;
-                    break;
-                case AppConfig.LevelOfAssurance.Substantial:
-                    level = 2;
-                    break;
-                case AppConfig.LevelOfAssurance.High:
-                    level = 3;
-                    break;
-            }
+            let level =  this.getLevelOfAssurance(credential.levelOfAssurance)
+            let stars = this.createStars(level);
 
-            let iconActive = "iconActive";
-            let iconInactive = "iconInactive";
-            let isActive = "isActive";
-            let iconStar = "icon-star";
-            let iconStarOutline = "icon-star-outline";
-
-            let stars = [{
-                [iconActive]: iconStar,
-                [iconInactive]: iconStarOutline,
-                [isActive]: true
-            }, {
-                [iconActive]: iconStar,
-                [iconInactive]: iconStarOutline,
-                [isActive]: true
-            }, {
-                [iconActive]: iconStar,
-                [iconInactive]: iconStarOutline,
-                [isActive]: true
-            }];
-
-            for (let z = 0; z < stars.length; z++) {
-                stars[z].isActive = ((z + 1 <= level) ? true : false);
-            }
-
-            let obj: mockCredential;
+            let credentialRes: Identity;
 
             if (this.isPresentationRequest) {
                 let securedCredentials;
                 let key = credential[AppConfig.FIELD_NAME];
-                return this.securedStrg.hasKey(AppConfig.CREDENTIAL_PREFIX + key)
-                    .then(result => {
-                        if (result) {
-                            return this.securedStrg.get(AppConfig.CREDENTIAL_PREFIX + key)
-                                .then((result) => {
-                                    securedCredentials = JSON.parse(result);
-                                    obj = {
-                                        id: count++,
-                                        titleP: credential[AppConfig.FIELD_NAME].toUpperCase().replace(/_/g, " "),
-                                        emitter: "Emisor del testimonio",
-                                        valueT: "Valor",
-                                        value: securedCredentials[key],
-                                        place: "Emisor de credencial",
-                                        addDateT: "Fecha incorporación del testimonio",
-                                        addDate: iatString,
-                                        endDateT: "Fecha fin de vigencia",
-                                        endDate: expString,
-                                        level: "Nivel " + level,
-                                        iconsStars: stars,
-                                        credentialAssigned: true,
-                                        isExpanded: false,
-                                        isHidden: false,
-                                        isChecked: true
-                                    };
-                                    this.identityDisplay.push(obj);
-                                    const credentialSelected: any = {
-                                        credential: this.credentials[obj.id],
-                                        index: obj.id
-                                    }
-                                    credentialSelected.credential[key] = securedCredentials[key];
-                                    this.loadCredential.emit(credentialSelected);
-                                    return Promise.resolve();
-                                });
-                        } else {
-                            obj = {
-                                id: count++,
-                                titleP: credential[propNames[2].toString()],
-                                emitter: "Emisor del testimonio",
-                                valueT: "Valor",
-                                value: "Credencial no selecionada o no disponible",
-                                place: "Emisor de credencial",
-                                addDateT: "Fecha incorporación del testimonio",
-                                addDate: "",
-                                endDateT: "",
-                                endDate: "",
-                                level: "Nivel " + level,
-                                iconsStars: stars,
-                                credentialAssigned: false,
-                                isExpanded: false,
-                                isHidden: false,
-                                isChecked: true
-                            };
-                            this.identityDisplay.push(obj);
-                            return Promise.resolve();
-                        }
-                    });
-            } else {
-                if (credential[AppConfig.IAT]) {
-                    iat = new Date(credential[AppConfig.IAT]);
-                    iatString = iat.getDay() + "/" + (iat.getMonth() + 1) + "/" + iat.getFullYear();
-                }
+                let hasKey = await this.securedStrg.hasKey(AppConfig.CREDENTIAL_PREFIX + key);
+                if (hasKey) {
+                    const resultKey = await this.securedStrg.get(AppConfig.CREDENTIAL_PREFIX + key)
+                    securedCredentials = JSON.parse(resultKey);
+                    credentialRes = this.parseCredential(count++, credential[AppConfig.FIELD_NAME], securedCredentials[key], iatString,
+                        expString, level, stars, true);
 
-                if (credential[AppConfig.EXP]) {
-                    exp = new Date(credential[AppConfig.EXP]);
-                    expString = exp.getDay() + "/" + (exp.getMonth() + 1) + "/" + exp.getFullYear();
+                    this.identityDisplay.push(credentialRes);
+                    const credentialSelected: any = {
+                        credential: this.credentials[credentialRes.id],
+                        index: credentialRes.id
+                    }
+                    credentialSelected.credential[key] = securedCredentials[key];
+                    this.loadCredential.emit(credentialSelected);
+                    return Promise.resolve();
+                } else {
+                    credentialRes = this.parseCredential(count++, credential[AppConfig.FIELD_NAME], null, '',
+                        'expString', level, stars, false);
+                    this.identityDisplay.push(credentialRes);
+                    return Promise.resolve();
                 }
-                obj = {
-                    id: count++,
-                    titleP: propNames[1].toUpperCase().replace(/_/g, " "),
-                    emitter: "Emisor del testimonio",
-                    valueT: "Valor",
-                    value: credential[propNames[1].toString()],
-                    place: "Emisor de credencial",
-                    addDateT: "Fecha incorporación del testimonio",
-                    addDate: iatString,
-                    endDateT: "Fecha fin de vigencia",
-                    endDate: expString,
-                    level: "Nivel " + level,
-                    iconsStars: stars,
-                    credentialAssigned: true,
-                    isExpanded: false,
-                    isHidden: false,
-                    isChecked: true
-                };
-                this.identityDisplay.push(obj);
+            } else {                
+                iatString = this.parseFormatDate(credential[AppConfig.IAT]);
+                expString = this.parseFormatDate(credential[AppConfig.EXP]);
+                    credentialRes = this.parseCredential(count++, propNames[1], credential[propNames[1].toString()], iatString,
+                    expString, level, stars, true);
+                this.identityDisplay.push(credentialRes);
                 return Promise.resolve();
             }
         });
@@ -242,43 +115,124 @@ export class IdentityDataListComponent {
             });
     }
 
-    public detail(item: any): void {
-        if (this.isPresentationRequest) {
-            this.securedStrg.getAllCredentials()
-                .then((credentials: any) => {
-                    
-                    let iat = this.navParams.get(AppConfig.IAT);
-                    let exp = this.navParams.get(AppConfig.EXP);
-
-                    new Promise((resolve) => {
-                        this.navCtrl.push(SelectIdentity, {
-                            item,
-                            isManualSelection: true,
-                            allCredentials: credentials,
-                            iat,
-                            exp,
-                            resolve
-                        })
-                    }).then((data: any) => {
-                        console.log('data ', data);
-                        if (data.mock && data.credential) {
-                            data.mock.isChecked = item.isChecked
-                            data.mock.id = item.id;
-                            this.identityDisplay[item.id] = data.mock;
-                            const result: any = {
-                                credential: data.credential,
-                                index: item.id
-                            }
-                            this.loadCredential.emit(result);
-                        }
-                    });
-                });
-        } else {
-            this.navCtrl.push(CredentialDetailPage, { item });
+    private parseFormatDate(date: any): string {
+        let result = '';
+        if (date) {
+            const newDate = new Date(date); 
+            result = newDate.getDay() + "/" + (newDate.getMonth() + 1) + "/" + newDate.getFullYear();
         }
+
+        return result;
     }
 
-    public expandCredential(item: mockCredential) {
+    private getLevelOfAssurance(credentialLevel: string): number{
+        let level: any = credentialLevel;
+        switch (level){
+            case AppConfig.LevelOfAssurance.Self:
+                level = 0;
+                break;
+            case AppConfig.LevelOfAssurance.Low:
+                level = 1;
+                break;
+            case AppConfig.LevelOfAssurance.Substantial:
+                level = 2;
+                break;
+            case AppConfig.LevelOfAssurance.High:
+                level = 3;
+                break;
+        }
+
+        return level;
+    }
+
+    private createStars(level: number): Array<any> {
+        let iconActive = "iconActive";
+        let iconInactive = "iconInactive";
+        let isActive = "isActive";
+        let iconStar = "icon-star";
+        let iconStarOutline = "icon-star-outline";
+
+        let stars = [{
+            [iconActive]: iconStar,
+            [iconInactive]: iconStarOutline,
+            [isActive]: true
+        }, {
+            [iconActive]: iconStar,
+            [iconInactive]: iconStarOutline,
+            [isActive]: true
+        }, {
+            [iconActive]: iconStar,
+            [iconInactive]: iconStarOutline,
+            [isActive]: true
+        }];
+
+        for (let z = 0; z < stars.length; z++) {
+            stars[z].isActive = ((z + 1 <= level) ? true : false);
+        }
+
+        return stars;
+    }
+
+    private parseCredential(id: number, title: string, value: any, addDate: string, endDate: string, level: number, stars: Array<any>, credentialAssigned: boolean) {
+
+        return {
+            id: id,
+            titleP: (title) ? title.toUpperCase().replace(/_/g, " ") : '',
+            emitter: "Emisor del testimonio",
+            valueT: "Valor",
+            value: (value) ? value : "Credencial no selecionada o no disponible",
+            place: "Emisor de credencial",
+            addDateT: "Fecha incorporación del testimonio",
+            addDate: addDate,
+            endDateT: "Fecha fin de vigencia",
+            endDate: endDate,
+            level: "Nivel " + level,
+            iconsStars: stars,
+            credentialAssigned: credentialAssigned,
+            isExpanded: false,
+            isHidden: false,
+            isChecked: true
+        };
+    }
+
+    public async detail(item: any): Promise<void> {
+        try {
+            if (this.isPresentationRequest) {
+                const credentials = await this.securedStrg.getAllCredentials()
+                let iat = this.navParams.get(AppConfig.IAT);
+                let exp = this.navParams.get(AppConfig.EXP);
+    
+                new Promise((resolve) => {
+                    this.navCtrl.push(SelectIdentity, {
+                        item,
+                        isManualSelection: true,
+                        allCredentials: credentials,
+                        iat,
+                        exp,
+                        resolve
+                    });
+                }).then((data: any) => {
+                    if (data.mock && data.credential) {
+                        data.mock.isChecked = item.isChecked
+                        data.mock.id = item.id;
+                        this.identityDisplay[item.id] = data.mock;
+                        const result: any = {
+                            credential: data.credential,
+                            index: item.id
+                        }
+                        this.loadCredential.emit(result);
+                    }
+                });
+            } else {
+                this.navCtrl.push(CredentialDetailPage, { item });
+            }
+        } catch (error) {
+            console.error('Detail error ', error);
+        }
+        
+    }
+
+    public expandCredential(item: Identity) {
         item.isExpanded = !item.isExpanded;
     }
 
@@ -297,13 +251,8 @@ export class IdentityDataListComponent {
                         this.identityDisplay[i][isChecked] = null;
                     }
                 }
-                this.handleIdentitySelect.emit(result);
-            } else if (this.chosenIndex === id) {
-                this.handleIdentitySelect.emit(result);
             }
         }
-        else {
-            this.handleIdentitySelect.emit(result);
-        }
+        this.handleIdentitySelect.emit(result);
     }
 }
